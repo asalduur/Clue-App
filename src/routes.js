7ef905bb48b702a82c1779b09fc9af18e38c08e1
michellegaletti <msgaletti@gmail.com>
2021-06-17 16:21:57 -0600

    Adding game back into the routes to allow display

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
-import {Switch, Route} from 'react-router-dom'
-import DiceRoll from './components/DiceRoll'
-// import Game from './components/Game'
-import Lobby from './components/Lobby'
+import { Switch, Route } from "react-router-dom";
+import DiceRoll from "./components/DiceRoll";
+import Game from "./components/Game";
+import Lobby from "./components/Lobby";
 // import Rules from './components/Rules'
 export default (
-    <Switch>
-        {/* <Route path='/game' component={Game} /> */}
-        <Route path='/lobby' component={Lobby} />
-        {/* <Route path='/rules' component={Rules} /> */}
-        <Route path='/diceroll' component={DiceRoll} /> {/* just making a path to get a display before the game component is done (where the dice roll will eventually be rendered)   */}
-
-    </Switch>
-)
+  <Switch>
+    <Route path="/game" component={Game} />
+    <Route path="/lobby" component={Lobby} />
+    {/* <Route path='/rules' component={Rules} /> */}
+    <Route path="/diceroll" component={DiceRoll} />{" "}
+    {/* just making a path to get a display before the game component is done (where the dice roll will eventually be rendered)   */}
+  </Switch>
+);
