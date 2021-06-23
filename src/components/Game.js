aa9d47f20b2ee6947bf5003e0abb3b99df1a0f9f
michellegaletti <msgaletti@gmail.com>
2021-06-23 12:09:49 -0600

    Adding suggest and accuse btn and adding styling to modals

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,18 +1,32 @@
 import GameBoard from "./GameBoard";
-import { useEffect, useContext } from "react";
+import { useState, useContext } from "react";
 import { SocketContext } from "../context/SocketContext";
 import Player from "./Player";
 import DiceRoll from "./DiceRoll";
+import AccuseSuggest from "../modals/AccuseSuggest";
 
 //import rooms, weapons, and people from sockets here
 
 const Game = () => {
   const { fakeplayer } = useContext(SocketContext);
+  const [suggestAccuse, setSuggestAccuse] = useState(false);
 
-  useEffect(() => {}, []);
+  const handleOpenSuggestAccuse = () => {
+    setSuggestAccuse(true);
+  };
+
   return (
     <>
       <h2 className="headerText">Hunch</h2>
+      <div className="suggestAccuseBtns">
+        <button className="modalBtnStyling" onClick={handleOpenSuggestAccuse}>
+          Suggest | Accuse
+        </button>
+        <AccuseSuggest
+          suggestAccuse={suggestAccuse}
+          setSuggestAccuse={setSuggestAccuse}
+        />
+      </div>
       <div className="flexGameboard">
         <GameBoard />
         <div className="sideBar">
