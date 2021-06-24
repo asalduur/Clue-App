fc4368c5615a5e7aef78cd163a675b90fba2dee2
casesensative <victor.waters1@gmail.com>
2021-06-24 12:37:11 -0400

    Core gameplay logic complete.

--- server/index.js
+++ server/index.js
@@ -6,11 +6,11 @@ const {SERVER_PORT} = process.env;
 let players = [];
 let tokens = ['blue', 'red', 'green', 'yellow'];
 let playerturn = 0;
-let playerArray = [];
+let case_file = {};
 
 const cards = [[
-    {weapon: 'IcePick'}, {weapon: 'Shank'}, {weapon: 'Coat Hanger'}, 
-    {weapon: 'Arsenic'}, {weapon: 'Blowtorch'}, {weapon: 'Ninja Star'}],
+    {weapon: 'Poisoned Cocktail'}, {weapon: 'Shank'}, {weapon: 'Coat Hanger'}, 
+    {weapon: 'Chainsaw'}, {weapon: 'Blowtorch'}, {weapon: 'Ninja Star'}],
   [
     {room: 'Grotto'}, {room: 'Pool-House'}, {room: 'Library'}, {room: 'Kitchen'}, 
     {room: 'Basement'}, {room: 'Wine Cellar'}, {room: 'Foyer'}, {room: 'Garden'}, 
@@ -25,45 +25,94 @@ app.use(express.json());
 
 const io = require('socket.io')(app.listen(SERVER_PORT, () => console.log('Server listening on ' + SERVER_PORT)), {cors: {origin: true}});
 
+const getOtherPlayers = (body) => {
+    let index = players.findIndex((p, i) => {
+      return p.id === body.id;
+    });
+    const firstplayers = [...players.slice(index + 1)];
+    const lastplayers = [...players.slice(0, index)];
+    const orderplayers = [...firstplayers, ...lastplayers];
+    return orderplayers;
+};
+
+const getActivePlayer = (body) => {
+  let aplayer = {};
+  players.forEach((p, i) => {
+    if (p.id === body.id) {
+      aplayer = p;
+    } 
+  })
+  return aplayer;
+}
+
 io.on('connection', (socket) => {
   console.log(`Socket ${socket.id} connected.`);
   tokens.sort(function() { return 0.5 - Math.random() });
+  console.log(tokens);
   switch (players.length) {
     case 0:
-      players.push({player: 'Player 1', id: socket.id, location: 'home', 
-                    roll: 0, token: tokens.splice(0, 1)[0]});
+      players.push({player: 1, id: socket.id, location: 'home', 
+                    roll: 0, token: tokens.splice(0, 1)});
+                    console.log(players);
       break;
     case 1:
-      players.push({player: 'Player 2', id: socket.id, location: 'home', 
-                    roll: 0, token: tokens.splice(0, 1)[0]});
+      players.push({player: 2, id: socket.id, location: 'home', 
+                    roll: 0, token: tokens.splice(0, 1)});
+                    console.log(players);
+      break;
+    case 2:
+      players.push({player: 3, id: socket.id, location: 'home', 
+                    roll: 0, token: tokens.splice(0, 1)});
+                    console.log(players);
       break;
     case 3:
-      players.push({player: 'Player 3', id: socket.id, location: 'home', 
-                    roll: 0, token: tokens.splice(0, 1)[0]});
-      break;
-    case 4:
-      players.push({player: 'Player 4', id: socket.id, location: 'home', 
-                    roll: 0, token: tokens.splice(0, 1)[0]});
+      players.push({player: 4, id: socket.id, location: 'home', 
+                    roll: 0, token: tokens.splice(0, 1)});
+                    console.log(players);
       break;
     default:
       socket.emit('Max players reached.');
       socket.disconnect();
   }
-  socket.on('disconnect', () => {
+  socket.on('disconnect', (body) => {
     console.log(`Socket ${socket.id} disconnected.`);
-    io.emit('player-disconnected', 'A player disconnected and the game has ended.');
+
+
+    if (players.length >= 3) {
+      let index = players.findIndex((p, i) => {
+        return p.id === socket.id;
+      });
+      const firstplayers = [...players.slice(index + 1)];
+      const lastplayers = [...players.slice(0, index)];
+      const orderedplayers = [...firstplayers, ...lastplayers];
+      console.log(orderedplayers);
+      players.splice(index, 1);
+
+      io.emit('update-players', players);
+      io.emit('player-start', orderedplayers[0]);
+    } else if (body.lost) {
+      io.emit('This game has ended. Please refresh to start a new game!');
+      io.disconnectSockets();
+      players = [];
+      tokens = ['blue', 'red', 'green', 'yellow'];
+    } else {
+      io.emit('player-disconnected', 'A player disconnected and the game has ended. Please refresh to start a new game.');
+      io.disconnectSockets();
+      players = [];
+      tokens = ['blue', 'red', 'green', 'yellow'];
+    }
+  });
+  socket.on('game reset', () => {
     io.disconnectSockets();
     players = [];
-  });
-  socket.on('say-hi', () => {
-    console.log('hello world');
+    tokens = ['blue', 'red', 'green', 'yellow'];
   });
   socket.on('send-message', (body) => {
     console.log(body);
     io.emit('relay-message', body);
   });
   socket.on('game-start', async () => {
-
+    if (players.length >= 3) { 
     players.forEach((e, i) => {
       switch (i) {
         case 0:
@@ -82,17 +131,17 @@ io.on('connection', (socket) => {
         default:
           null;
       }
-    }) 
-    players.sort(function() { return 0.5 - Math.random() });
+    });
+    let playercopy = [...players];
+    playercopy.sort(function() { return 0.5 - Math.random() });
 
     let weaponArr = [...cards[0]];
     let roomArr = [...cards[1]];
     let peopleArr = [...cards[2]];
-    const murder_weapon = weaponArr.splice(Math.floor(Math.random() * weaponArr.length - 1), 1);
-    let murder_perp = peopleArr.splice(Math.floor(Math.random() * peopleArr.length - 1), 1);
-    let murder_room = roomArr.splice(Math.floor(Math.random() * roomArr.length - 1), 1);
-    let case_file = [murder_weapon, murder_perp, murder_room];
-    console.log(case_file);
+    const [murder_weapon] = weaponArr.splice(Math.floor(Math.random() * weaponArr.length), 1);
+    const [murder_perp] = peopleArr.splice(Math.floor(Math.random() * peopleArr.length), 1);
+    const [murder_room] = roomArr.splice(Math.floor(Math.random() * roomArr.length), 1);
+    case_file = {...murder_weapon, ...murder_perp, ...murder_room};
     let cardArray = [...weaponArr, ...roomArr, ...peopleArr];
 
     cardArray.sort(function() { return 0.5 - Math.random() });
@@ -119,16 +168,12 @@ io.on('connection', (socket) => {
 
       }
     }
-
-    console.log(players);
-
     io.emit('update-players', players);
-
-
-
-
-
-    io.emit('player-start', players[playerturn]);
+    io.emit('case-file', case_file)
+    io.emit('player-start', playercopy[playerturn]);
+  } else {
+    io.emit('more-players');
+  }
 
   });
 
@@ -137,40 +182,84 @@ io.on('connection', (socket) => {
       if (p.id === body.id) {
         p.roll += body.rollvalue;
       }
-      // if (p.roll > 20) {
-      //   io.emit('room-choose', p);
-      //   // e.roll = 0;
-      // }
     });
     io.emit('update-players', players);
     players.forEach((p, i) => {
-      if (p.roll > 20) {
-        io.emit('room-choose', p);
-
-        // e.roll = 0;
-      }
-    })
-  })
+      if (p.id === body.id) {
+        if (p.roll >= 20) {
+          io.emit('room-choose', p);
+          p.roll = 0;
+        } else {
+          const orderedplayers = getOtherPlayers(body);
+          io.emit('player-start', orderedplayers[0]);
+        }
+    }
+    });
+  });
 
   socket.on('send-location', (body) => {
-    playerArray.forEach((p, i) => {
-      if (p.id === body.id) {
-        p.location = body.location
-      }
-    })
+    const activeplayer = getActivePlayer(body);
     io.emit('update-players', players);
-    io.emit('accuse-suggest', body.id);
+    io.emit('accuse-suggest', activeplayer);
+  });
+
+  socket.on('send-suggestion', (body) => {
+    const activeplayer = getActivePlayer(body);
+    io.emit('suggest-message', {activeplayer, body});
+    const orderedplayers = getOtherPlayers(body);
+    let othercards = [];
+    orderedplayers.forEach((player, i) => {
+      player.cards.forEach((card, i) => {
+        for (const prop in card) {
+          if (body[prop]) {
+            if (card[prop] === body[prop]) {
+              othercards.push({player, card});
+            }
+          }
+        }
+      })
+    })
+    othercards.sort(function() { return 0.5 - Math.random() });
+    let othercard = othercards[0];
+    if (othercard) {
+      othercard.aplayer = activeplayer;
+      setTimeout(() => {
+        io.emit('send-proof', othercard);
+      }, 7000)
+      // io.emit('send-proof', othercards[0]);
+    } else {
+      setTimeout(() => {
+        io.emit('accuse-choice', activeplayer);
+      }, 7000)
+      
+    }
+
+  });
+
+  socket.on('suggest-finished', (player) => {
+    const orderedplayers = getOtherPlayers(body);
+    io.emit('player-start', orderedplayers[0]);
+  });
+
+  socket.on('send-accusation', (body) => {
+    const activeplayer = getActivePlayer(body);
+    const otherplayers = getOtherPlayers(body);
+    if (body.room === case_file.room && 
+        body.weapon === case_file.weapon && 
+        body.suspect === case_file.suspect) {
+          io.emit('game-win', {body, player: activeplayer.player})
+        } else {
+          io.emit('player-lost', {body, player: activeplayer.player});
+        }
+  });
+  socket.on('end-turn', (body) => {
+    io.emit('update-messages');
+    const orderedplayers = getOtherPlayers(body);
+    io.emit('player-start', orderedplayers[0]);
   })
-  
-  // console.log(players);
 })
 
 
-
-
-
-
-//minimum players to start game
 
 
 
--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -1,35 +1,39 @@
-import { createContext, useState, useEffect, useCallback, useRef } from "react";
-import io from "socket.io-client";
+import {createContext, useState, useEffect, useRef} from 'react';
+import io from 'socket.io-client';
 
 export const SocketContext = createContext();
 
 export const SocketProvider = (props) => {
+
   const [socket, setSocket] = useState(null);
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] =useState([]);
   const [player, setPlayer] = useState(null);
   const [active, setActive] = useState(false);
   const [activeRoll, setActiveRoll] = useState(false);
   const [activeRoom, setActiveRoom] = useState(false);
+  const [activeSA, setActiveSA] = useState(false);
+  const [activeAccuse, setActiveAccuse] = useState(false);
+  const [casefile, setCaseFile] = useState(null);
+  const [suggestion, setSuggestion] = useState(null);
+  const [accusation, setAccusation] = useState(null);
+  const [lossmsg, setLossMsg] = useState(null);
+  const [winmsg, setWinMsg] = useState(null);
+  const [proofmsg, setProofMsg] = useState(null);
+  const [resetmsg, setResetMsg] = useState(null);
+  const [playerlost, setPlayerLost] = useState(false);
+  const [playerwin, setPlayerWin] = useState(false);
   const [inactiveRoll, setInactiveRoll] = useState(true);
-  const [inactiveMsg, setInactiveMsg] = useState("");
-  const [fakeplayer, setFakePlayer] = useState({
-    player: "Player 1",
-    id: "O09aIvQ6mAvYpasGAAAH",
-    location: "foyer",
-    roll: 0,
-    token: "green",
-    cards: [],
-  });
-  const [currentRoom, setCurrentRoom] = useState("foyer");
+  const [inactiveMsg, setInactiveMsg] = useState('');
+  const [fakeplayer, setFakePlayer] = useState({player: 'Player 1', 
+  id: 'O09aIvQ6mAvYpasGAAAH', location: 'home', roll: 0, token: 'green', cards: []});
 
   const playerRef = useRef(player);
 
   useEffect(() => {
     playerRef.current = player;
-  }, [player]);
+  }, [player])
 
-  let vplayer = {};
-  let vactive = false;
+
 
   useEffect(() => {
     if (!socket) {
@@ -38,78 +42,170 @@ export const SocketProvider = (props) => {
     return () => {
       socket.disconnect();
       setSocket(null);
-    };
+    }
   }, []);
+
+
 
   useEffect(() => {
     if (socket) {
-      socket.on("relay-message", (body) => {
+      socket.on('relay-message', (body) => {
         console.log(body.message);
         setMessages((currentstate) => [...currentstate, body]);
       });
-      socket.on("player-disconnected", (msg) => {
+      socket.on('player-disconnected', (msg) => {
         alert(msg);
       });
 
-      socket.on("update-players", (players) => {
-        console.log("PLAYERS O ID;", players[0].id);
-        console.log("SOCKET ID:", socket.id);
+      socket.on('update-players', (players) => {
+        setPlayer(null);
         players.forEach((p, i) => {
           if (p.id === socket.id) {
-            console.log("P:", p);
+            console.log('P:', p);
             setPlayer(p);
-            vplayer = p;
-            console.log(vplayer);
           }
-        });
+        })
+      })
+      socket.on('case-file', (casefile) => {
+        setCaseFile(casefile);
       });
-      socket.on("player-start", (start_player) => {
+      socket.on('player-start', (start_player) => {
+        setLossMsg(null);
         if (start_player.id === socket.id) {
           setActive(true);
-          vactive = true;
           setActiveRoll(true);
         } else {
-          setInactiveMsg(`${start_player.player} is currently rolling.`);
-        }
-      });
-      socket.on("room-choose", (a_player) => {
-        console.log("PLAYER:", playerRef.current);
-        console.log("ACTIVE_PLAYER:", a_player);
-        if (playerRef.current.id === a_player.id) {
+          setActive(false);
           setActiveRoll(false);
-          setActiveRoom(true);
-        } else {
-          setInactiveMsg(`${a_player.player} is currently choosing a room.`);
+          setInactiveMsg(`Player ${start_player.player} is currently rolling.`);
         }
-      });
-    }
+        
+      })
+      socket.on('room-choose', (a_player) => {
+        console.log(a_player);
+            if (playerRef.current.id === a_player.id) {
+              setActiveRoll(false);
+              setActiveRoom(true);
+            } else {
+              setInactiveMsg(`Player ${a_player.player} is currently choosing a room.`);
+            }});
+      socket.on('accuse-suggest', (a_player) => {
+        if (playerRef.current.id === a_player.id) {
+          setActiveRoom(false);
+          setActiveSA(true);
+        } else {
+          setInactiveMsg(`Player ${a_player.player} is currently making a suggestion or accusation.`)
+        }
+      })
+      socket.on('send-proof', (p) => {
+        const {aplayer, player, card} = p;
 
-    // endpoints
+        setSuggestion(null);
+        if (card.suspect) {
+          setProofMsg(`Player ${player.player} proves beyond a shadow of doubt 
+          the ${card.suspect} wasn't involved!`);
+        } else if (card.room) {
+          setProofMsg(`Player ${player.player} proves beyond a shadow of doubt 
+          that it didn't happen in the ${card.room}!`);
+        } else if (card.weapon) {
+          setProofMsg(`Player ${player.player} proves beyond a shadow of doubt 
+          that a ${card.weapon} couldn't have been the murder weapon!`);
+        }
+        setTimeout(() => {
+          setProofMsg(null);
+          socket.emit('end-turn', aplayer);
+        }, 7000)
+        
+      })
+      socket.on('player-lost', (data) => {
+        const {body} = data;
+        setLossMsg(`You lost while accusing the ${body.suspect} of murdering Mr. Boddy in the ${body.room} with a ${body.weapon}!`);
+        setInactiveMsg(`Player ${data.player} lost while accusing the ${body.suspect} of murdering Mr. Boddy in the ${body.room} with a ${body.weapon}!`);
+        setTimeout(() => {
+          if (body.id === socket.id) {
+            setPlayerLost(true);
+            socket.disconnect({lost: true});
+          }
+        }, 7000);
+      });
+      socket.on('game-win', (data) => {
+        const {body} = data;
+        setInactiveMsg(`Player ${body[1].player} accused ${body[0].suspect} of murdering Mr. Boddy in the ${body[0].room} with a ${body[0].weapon}, and WON!`);
+        setWinMsg(`You accused ${body[0].suspect} of murdering Mr. Boddy in the ${body[0].room} with a ${body[0].weapon}, and WON!`);
+        setTimeout(() => {
+          if (body.id !== socket.id) {
+            setPlayerLost(true);
+          } else {
+            setPlayerWin(true);
+          }
+          setResetMsg(`Player ${body[1].player} has won and the game has ended. Please refresh your browser to join a new game.`)
+          socket.emit('game-reset');
+        }, 7000)
+      })
+      socket.on('suggest-message', (data) => {
+        const {activeplayer, body} = data;
+        if (playerRef.current.id !== activeplayer.id) {
+          setInactiveMsg(`Player ${activeplayer.player} is suggesting that the ${body.suspect} killed Mr. Boddy in the ${body.room} with a ${body.weapon}.`);
+        }
+      })
+      socket.on('accuse-choice', (player) => {
+        if (playerRef.current.id === player.id) {
+          setActiveSA(false);
+          setActiveAccuse(true);
+
+        } else {
+          setInactiveMsg(`No one could prove the suggestion wrong. Player ${player.player} is deciding whether or not to make an accusation.`)
+        }
+      })
+      socket.on('update-messages', () => {
+        setInactiveMsg(null);
+      })
+
+    }
   }, [socket]);
 
+
   const sayHi = () => {
-    socket.emit("say-hi");
-  };
+    socket.emit('say-hi');
+  }
 
   const sendMessage = (message) => {
-    socket.emit("send-message", { message });
-  };
+    socket.emit('send-message', {message})
+  }
 
   const testFunction = () => {
-    socket.emit("game-start");
-  };
+    socket.emit('game-start');
+  }
 
   const sendRoll = (rollvalue) => {
-    if (active && activeRoll) {
-      socket.emit("send-roll", { id: socket.id, rollvalue: rollvalue });
-    }
-  };
+     if (active && activeRoll) {
+    socket.emit('send-roll', {id: socket.id, rollvalue: rollvalue})
+  }
+  }
 
   const sendRoom = (room) => {
     if (active && activeRoom) {
-      socket.emit("send-location", { id: socket.id, location: room });
+      socket.emit('send-location', {id: socket.id, location: room})
     }
-  };
+  }
+
+  const sendSuggest = (room, weapon, suspect) => {
+    if (active && activeSA) {
+      socket.emit('send-suggestion', {id: socket.id, room, weapon, suspect});
+    }
+  }
+
+  const sendAccusation = (room, weapon, suspect) => {
+    if ((active && activeSA) || (active && activeAccuse)) {
+      setSuggestion(null);
+      socket.emit('send-accusation', {id: socket.id, room, weapon, suspect});
+    }
+  }
+
+  const endTurn = () => {
+
+    socket.emit('end-turn', {id: socket.id});
+  }
 
   console.log(vplayer);
 
@@ -126,10 +222,17 @@ export const SocketProvider = (props) => {
         fakeplayer,
         currentRoom,
         setCurrentRoom,
+        inactiveMsg,
         sendRoom,
-      }}
-    >
+        sendSuggest,
+        endTurn,
+        sendAccusation,
+        lossmsg,
+        winmsg,
+        suggestion,
+        proofmsg
+      }}>
       {props.children}
-    </SocketContext.Provider>
-  );
-};
+    </SocketContext.Provider>  
+  )
+}
