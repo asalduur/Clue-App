6decf8e9bb21f91d46eb0a8a89a7457e84a157e1
casesensative <victor.waters1@gmail.com>
2021-06-30 20:56:22 -0400

    added player legend

--- src/components/Game.js
+++ src/components/Game.js
@@ -13,7 +13,7 @@ import Rules from "../modals/Rules";
 import { useHistory } from 'react-router-dom';
 
 const Game = () => {
-  const { roomId } = useContext(SocketContext);
+  const { roomId, players, player } = useContext(SocketContext);
   const [modalOpen, setModalOpen] = useState(false);
   const [ruleModalOpen, setRuleModalOpen] = useState(false);
 
@@ -23,12 +23,21 @@ const Game = () => {
     history.push('/');
   }
 
+  let opponentpieces = [];
+
+  players.filter((p, i) => {
+    if (p.id !== player.id) {
+      opponentpieces.push(p);
+    }
+  });
+
   return roomId ? (
     <>
       <div className="headerBar">
         <h2 className="headerText">Hunch</h2>
       </div>
-      <div className="suggestAccuseBtns">
+      <div className='topbar' style={{display: 'flex', width: '100vw', justifyContent: 'center'}}>
+      <div className="suggestAccuseBtns" style={{flex: '1', display: 'flex', justifyContent: 'flex-start', marginLeft: '3.3%', height: '30px'}}>
         <button
           onClick={() => setModalOpen(!modalOpen)}
           className="modalBtnStyling"
@@ -36,6 +45,18 @@ const Game = () => {
           Suggest/Accuse
         </button>
         <AccuseSuggest modalOpen={modalOpen} setModalOpen={setModalOpen} />
+        </div>
+        <div className='opponentlegend' style={{display: 'flex', flex: '3', justifyContent: 'flex-end', marginRight: '21%', height: '30px'}}>
+        {opponentpieces.map((o, i) => {
+        return (
+          <span style={{display: 'flex', justifyContent: 'space-between', 
+          marginLeft: '10px', marginRight: '10px'}}>
+            <p>Player {o.player}: </p>
+            <p>{o.token}</p>
+          </span>
+        )
+      })}
+        </div>
       </div>
       <div className="flexGameboard">
         <GameBoard modalOpen={modalOpen} setModalOpen={setModalOpen} />
